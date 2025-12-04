8036c54f39b221ab608bba7c641eeacd135aa044
Alexander Bonnet <62345566+alexanderbonnet@users.noreply.github.com>
2025-12-04 18:23:58 -0500

    add workflow to publish (#12)

--- esmjax/esm2/_tokenizer.py
+++ esmjax/esm2/_tokenizer.py
@@ -1,24 +1,25 @@
-import pathlib
 from typing import Final
 
 import jax.numpy as jnp
 from beartype import beartype
 from jaxtyping import Array, Bool, Int, jaxtyped
 
-
-def _load_vocab() -> dict[str, int]:
-    vocab_path = pathlib.Path(__file__).parent / "_vocab.txt"
-    vocab = vocab_path.read_text().splitlines()
-    return {token: idx for idx, token in enumerate(vocab)}
-
-
-VOCAB: Final[dict[str, int]] = _load_vocab()
+# fmt: off
+VOCAB: Final[list[str]] = [
+    "<cls>", "<pad>", "<eos>", "<unk>", "L", "A", "G", "V", "S", "E", "R",
+    "T", "I", "D", "P", "K", "Q", "N", "F", "Y", "M", "H", "W", "C", "X",
+    "B", "U", "Z", "O", ".", "-", "<null_1>", "<mask>"
+    ]
+# fmt: on
+TOKENS: Final[dict[str, int]] = dict(zip(VOCAB, range(len(VOCAB))))
 
 
 @jaxtyped(typechecker=beartype)
 def tokenize(sequence: str) -> Int[Array, " n"]:
     tokens = (
-        [VOCAB["<cls>"]] + [VOCAB.get(char, VOCAB["<unk>"]) for char in sequence] + [VOCAB["<eos>"]]
+        [TOKENS["<cls>"]]
+        + [TOKENS.get(char, TOKENS["<unk>"]) for char in sequence]
+        + [TOKENS["<eos>"]]
     )
     return jnp.array(tokens, dtype=jnp.int32)
 
